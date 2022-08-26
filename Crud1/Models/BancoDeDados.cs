8e17808dc54fcc199b4483652b65023fa5f29e83
AnnaVFernandes <anna.v.fernandes@gmail.com>
2022-08-25 21:32:00 -0300

    primeiro commit

---
+++ Crud1/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud1.Models
+{
+    public class BancoDeDados : DbContext
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+
+    {
+        optionsBuilder.UseSqlServer(connectionString:);
+    }
+}
